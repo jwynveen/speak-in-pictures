302fec97f01ac968562bd4dac6014255c6413175
Jon Wynveen <jonwynveen@gmail.com>
2017-09-10 11:42:55 +0200

    Search page layout

--- config/routes.js
+++ config/routes.js
@@ -4,5 +4,6 @@ module.exports = {
   'get /': 'HomeController.index',
   'get /kitchen-sink': 'HomeController.kitchenSink',
   'get /modern-browser': 'HomeController.modernBrowser',
+  'get /search': 'SearchController.index',
   'get /meaning/:id': 'MeaningController.detail',
 };
